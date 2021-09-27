957441b35a9bc37cfd2a0353a1c38b9f00e31f74
yooil-405 <yooil405@gmail.com>
2021-09-28 08:53:31 +0900

    20210927 수업복습

---
+++ 수업 복습/Oracle DataBase/04_FUNCTION.sql
@@ -0,0 +1,129 @@
+/*
+    <함수>
+        컬럼값을 읽어서 계산한 결과를 반환한다.
+            - 단일행 함수 : N 개의 값을 읽어서 N 개의 결과를 리턴한다. (매 행 함수 실행 결과 반환)
+            - 그룹 함수 : N 개의 값을 읽어서 1개의 결과를 리턴한다. (하나의 그룹별로 함수 실행 결과 반환)
+            - SELECT 절에 단일행 함수와 그룹함수를 함께 사용하지 못 한다. (결과 행의 개수가 다르기 때문)
+            - 함수를 기술할 수 있는 위치는 SELECT, WHERE, ORDER BY, GROUP BY, HAVING 절에 기술할 수 있다.
+*/
+
+--------------------------------------- 단일행 함수 ----------------------------------
+
+-- LENGTH / LENGTHB
+-- DUAL 테이블 : sys가 소유하고 있는 테이블이며 모든 사용자가 접근 가능. 한 행, 한 컬럼을 가지고 있는 더미(DUMMY) 테이블이며 계산시 임시로 사용
+
+SELECT LENGTH('오라클'), LENGTHB('오라클')
+FROM DUAL;
+
+SELECT LENGTH('SHOWMETHEMONEY'), LENGTH('SHOW ME THE MONEY'), LENGTHB('SHOWMETHEMONEY'), LENGTHB('SHOW ME THE MONEY')
+FROM DUAL;
+
+SELECT EMP_NAME, LENGTHB(EMP_NAME), PHONE, LENGTH(PHONE)
+FROM EMPLOYEE;
+
+-- INSTR : 지정한 위치부터 지정된 숫자 번째로 나타난는 문자의 시작 위치를 반환한다.
+
+SELECT INSTR('AABAACAABBAA', 'B')
+FROM DUAL;
+
+SELECT INSTR('AABAACAABBAA', 'B', 1, 2)
+FROM DUAL;
+
+SELECT INSTR('AABAACAABBAA', 'B', 1, 3)
+FROM DUAL;
+
+SELECT INSTR('AABAACAABBAA', 'B', -1, 1)
+FROM DUAL;
+
+SELECT INSTR('AABAACAABBAA', 'B', -1, 2)
+FROM DUAL;
+
+SELECT INSTR('AABAACAABBAA', 'B', -1, 3)
+FROM DUAL;
+
+-- LPAD /  RPAD
+
+-- 20만큼의 길이 중 EMAIL 값은 오른쪽으로 정렬하고 공백을 왼쪽으로 채운다.
+SELECT LPAD(EMAIL, 20)
+FROM EMPLOYEE;
+
+SELECT LPAD(EMAIL, 10)
+FROM EMPLOYEE;
+
+SELECT RPAD(EMAIL, 10)
+FROM EMPLOYEE;
+
+-- 991212-2******을 출력
+SELECT RPAD('991212-2', 14, '*')
+FROM DUAL;
+
+SELECT RPAD(SUBSTR('991212-2222222', 1, 8), 14, '*')
+FROM DUAL;
+
+-- LTRIM / RTRIM
+SELECT LTRIM('     KH')
+FROM DUAL;
+
+SELECT LTRIM('00045648787', '0')
+FROM DUAL;
+
+SELECT LTRIM('123123451', '123')
+FROM DUAL;
+
+SELECT LTRIM('123123451123', '123')
+FROM DUAL;
+
+SELECT RTRIM('KH     ')
+FROM DUAL;
+
+SELECT RTRIM('001230000456000', '0')
+FROM DUAL;
+
+-- TRIM
+SELECT TRIM('       KH     ')
+FROM DUAL;
+
+SELECT TRIM( 'Z' FROM 'ZZZZKHZZZZ')
+FROM DUAL;
+
+SELECT TRIM( BOTH 'Z' FROM 'ZZZZKHZZZZ')
+FROM DUAL;
+
+SELECT TRIM(LEADING 'Z' FROM 'ZZZZKHZZZZ')
+FROM DUAL;
+
+SELECT TRIM(TRAILING 'Z' FROM 'ZZZZKHZZZZ')
+FROM DUAL;
+
+-- SUBSTR
+SELECT SUBSTR('SHOWMETHEMONEY' , 7) FROM DUAL;
+SELECT SUBSTR('SHOWMETHEMONEY' , 3, 8) FROM DUAL;
+SELECT SUBSTR('SHOWMETHEMONEY' , 1, 6) FROM DUAL;
+SELECT SUBSTR('SHOWMETHEMONEY' , -8, 3) FROM DUAL;
+
+-- LOWER / UPPER / INITCAP
+
+SELECT LOWER('Welcome To My World!') FROM DUAL;
+SELECT UPPER('Welcome To My World!') FROM DUAL;
+SELECT INITCAP('welcome to my world!') FROM DUAL;
+
+-- CONCAT
+SELECT CONCAT('안녕하', 'ABC') "콘켓" FROM DUAL;
+SELECT '안녕하' || 'ABC' FROM DUAL;
+-- SELECT CONCAT('안녕하', 'ABC', '세요') FROM DUAL;
+SELECT '안녕하' || 'ABC' || '세요' FROM DUAL;
+
+-- REPLACE
+SELECT REPLACE('인천시 연수구 송도동', '송도동', '동춘동') FROM DUAL; 
+
+
+
+
+
+
+
+
+
+
+
+            
