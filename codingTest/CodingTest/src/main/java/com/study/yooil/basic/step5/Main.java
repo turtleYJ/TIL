4dc69e68d9571185748c61ca293317227f9d0266
yooil-405 <yooil405@gmail.com>
2022-03-13 06:04:48 +0900

    20220313 javaScript공부 및 코테문제풀이 update

--- codingTest/CodingTest/src/main/java/com/study/yooil/basic/step5/Main.java
+++ codingTest/CodingTest/src/main/java/com/study/yooil/basic/step5/Main.java
@@ -1,12 +1,9 @@
 package com.study.yooil.basic.step5;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 
@@ -111,59 +108,242 @@ public class Main {
 //		}
 		
 		// 4. 나머지
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int i = 0;
-		int temp = 0;
-		int count = 0;
-		int[] N1 = new int[10];
-		int[] N2 = new int[10];
-		int[] remainder = new int[42];
-		
-		while(i < 10) {
-			try {
-				N1[i] = Integer.parseInt(br.readLine());
-				
-			} catch (NumberFormatException | IOException e) {
-				e.printStackTrace();
-			}
-			i++;
-		}
-		
-		i = 0;
-		
-		while(i < 10) {
-			N2[i] = N1[i] % 42;
-			i++;
-		}
-		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		
+//		int i = 0;
+//		int temp = 0;
+//		int count = 0;
+//		int[] N1 = new int[10];
+//		int[] N2 = new int[10];
+//		int[] remainder = new int[42];
+//		
+//		while(i < 10) {
+//			try {
+//				N1[i] = Integer.parseInt(br.readLine());
+//				
+//			} catch (NumberFormatException | IOException e) {
+//				e.printStackTrace();
+//			}
+//			i++;
+//		}
+//		
 //		i = 0;
 //		
 //		while(i < 10) {
-//			System.out.println(N2[i]);
+//			N2[i] = N1[i] % 42;
 //			i++;
 //		}
+//		
+////		i = 0;
+////		
+////		while(i < 10) {
+////			System.out.println(N2[i]);
+////			i++;
+////		}
+//		
+//		i = 0;
+//		
+//		while(i < 10) {
+//			temp = N2[i];
+//			remainder[temp]++;
+//			i++;			
+//		}
+//		
+//		i = 0;
+//		
+//		while(i < 42) {
+//			if(remainder[i] != 0) {
+//				count++;
+//			}			
+//			i++;
+//		}
+//		
+//		System.out.println(count);
+//		
+//		// 5. 평균
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		
+//		int N = 0;
+//		double max = 0;
+//		double result = 0;
+//		double sum = 0;
+//		
+//		try {
+//			N = Integer.parseInt(br.readLine());
+//			if(N <= 1000) {
+//				String[] score = new String[N];
+//				Double[] intScore = new Double[N];
+//				score = br.readLine().split(" ");
+//				
+//				
+//				
+//				// 대입 반복문
+//				for(int i = 0; i < N; i++) {
+//					intScore[i] = Double.parseDouble(score[i]); 
+//				}
+//				
+//				Arrays.sort(intScore, Collections.reverseOrder());
+//				
+//				max = intScore[0];
+//				
+//				// 계산 및 대입 반복문
+//				for(int i = 0; i < N; i++) {
+//					intScore[i] = intScore[i] / max * 100;
+//					
+//					sum += intScore[i];
+//				}
+//				
+//				result = sum / N;
+//				
+//				System.out.println(Arrays.toString(intScore));
+//				System.out.println(result);
+//			} else {
+//				System.out.println("1000보다 적은 수를 입력하세요");
+//			}
+//			
+//		} catch (NumberFormatException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
-		i = 0;
 		
-		while(i < 10) {
-			temp = N2[i];
-			remainder[temp]++;
-			i++;			
+//		// 5. 평균(re)
+//		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		try {
+//			double arr[] = new double[Integer.parseInt(br.readLine())];
+//			
+//			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//			
+//			for(int i = 0; i < arr.length; i++) {
+//				arr[i] =  Double.parseDouble(st.nextToken());
+//			}
+//			
+//			double sum = 0;
+//			Arrays.sort(arr);
+//			
+//			for(int i = 0; i < arr.length; i++) {
+//				sum += arr[i] / arr[arr.length - 1] * 100;
+//			}
+//			
+//			System.out.println(sum/arr.length);
+//			
+//		} catch (NumberFormatException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	
+		
+//		// 6. OX퀴즈
+//	
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		try {
+//			
+//			int N = Integer.parseInt(br.readLine());
+//			int[] sum = new int[N];
+//			int temp = 0;
+//		
+//			String[] arr = new String[N];
+//			for(int i = 0; i < arr.length; i++) {
+//				arr[i] = br.readLine();
+//				
+//				temp = 0;
+//				
+//				for(int j = 0; j < arr[i].length(); j++) {
+//					if(arr[i].charAt(j) == 'O') {
+//						temp += 1;
+//						
+//						sum[i] += temp;						
+//					} else if(arr[i].charAt(j) == 'X') {
+//						temp = 0;
+//					}
+//				}
+//			}
+//			
+//			for (int i : sum) {
+//				System.out.println(i);
+//			}
+//			
+//		} catch (NumberFormatException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		// 7. 평균은 넘겠지
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			int N = Integer.parseInt(br.readLine());
+			
+			String[] arr = new String[N];
+			int[] size = new int[N];
+			int[][] arr2 = null;
+			double[] sum = new double[N];
+			double[] avg = new double[N];
+			double[] result = new double[N];
+			double[] count = new double[N];
+			
+			for(int i = 0; i < N; i++) {
+				arr[i] = br.readLine();
+				
+				StringTokenizer st = new StringTokenizer(arr[i], " ");
+				
+				size[i] = Integer.parseInt(st.nextToken());				
+				
+				arr2 = new int[N][size[i]];
+				
+				for(int j = 0; j < size[i]; j++) {
+					arr2[i][j] = Integer.parseInt(st.nextToken());
+					
+					sum[i] += arr2[i][j];
+				}
+				
+				avg[i] = sum[i] / size[i];
+				
+				for(int j = 0; j < size[i]; j++) {
+					if(arr2[i][j] > avg[i]) {
+						count[i] += 1;
+					}
+				}
+				
+//				System.out.println(count);
+				
+				result[i] = count[i] / size[i] * 100;
+				
+//				System.out.println(avg);
+//				System.out.println(result);
+//				System.out.println(Arrays.toString(arr2));
+				
+				
+			}
+
+			for (double d : result) {
+				System.out.println(String.format("%.3f", d) + "%");
+			}
+			
+
+		
+			
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		i = 0;
-		
-		while(i < 42) {
-			if(remainder[i] != 0) {
-				count++;
-			}			
-			i++;
-		}
-		
-		System.out.println(count);
-	}
 
 	
 	
@@ -184,7 +364,7 @@ public class Main {
 	
 	
 	
-	
+	}
 	
 	
 }
