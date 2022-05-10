b4d9f3c6716d6370609d1fa466006efbcb43f95f
yooil-405 <yooil405@gmail.com>
2022-05-10 16:57:28 +0900

    20220510 백준문제풀이_17478(recursion)

--- codingTest/CodingTest/src/main/java/com/study/yooil/BAEKJOON/step9/Main.java
+++ codingTest/CodingTest/src/main/java/com/study/yooil/BAEKJOON/step9/Main.java
@@ -8,6 +8,8 @@ public class Main {
 	static char[][] arr;
 	static StringBuilder sb = null;
 	static int count = 0;
+	static int N;
+	static String tab = "";
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -61,99 +63,129 @@ public class Main {
 //		
 //		System.out.println(sb);
 		
-		// 4. 하노이 탑 이동 순서(*****)
-		int N = 0;
-		int result = 0;
+//		// 4. 하노이 탑 이동 순서(*****)
+//		int N = 0;
+//		int result = 0;
+//		
+//		sb = new StringBuilder();
+//		
+//		N = Integer.parseInt(br.readLine());
+//		
+//		hanoi(N, 1, 3, 2);
+//		
+//		System.out.println(count);
+//		
+//		System.out.println(sb);
 		
-		sb = new StringBuilder();
-		
+		// 여기서 부터 문제번호로 적음.
+		// 17478 - 재귀함수가 뭔가요?
 		N = Integer.parseInt(br.readLine());
 		
-		hanoi(N, 1, 3, 2);
+		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+		recur17478(N, "");
 		
-		System.out.println(count);
 		
-		System.out.println(sb);
+		
+		
 	}
 	
-	
-
-	
-	private static void hanoi(int N, int from, int to, int remain) {
+	private static void recur17478(int time, String tap) {
+		System.out.println(tab + "\"재귀함수가 뭔가요?\"");
 		
+		String tabLocal = tab;
 		
-		if(N == 0) return;
-		
-		count++;
-		
-		// 바로 전전
-		hanoi(N-1, from, remain, to);
-		sb.append(from + " " + to + "\n");
-		// 바로 전
-		hanoi(N-1, remain, to, from);
-	}
-
-
-
-
-	private static void starMaker(int x, int y, int N, boolean blank) {
-		// 공백인 경우
-		if(blank) {
-			for(int i = x; i < x + N; i++) {
-				for(int j = y; j < y + N; j++) {
-					arr[i][j] = ' ';
-				}
-			}
-			
-			return;
-		}
-		
-		// 더이상 쪼갤 수 없을 때
-		if(N == 1) {
-			arr[x][y] = '*';
-			
-			return;
-		}
-		
-		
-		// 기본
-		int size = N / 3;
-		int count = 0;
-		for (int i = x; i < x + N; i += size) {
-			for (int j = y; j < y + N; j += size) {
-				count++;
-				if (count == 5) { // 공백 칸일 경우
-					starMaker(i, j, size, true);
-				} else {
-					starMaker(i, j, size, false);
-				}
-			}
-		}
-		
-		
-	}
-
-
-	private static int Fibonacci(int n) {
-		if(n == 0) {
-			return 0;
-		} else if(n == 1) {
-			return 1;
-		}
-		
-		return Fibonacci(n-1) + Fibonacci(n-2);
-	}
-
-
-	// 재귀 함수
-	public static int factorial(int n) {
-		if(n <= 1) {
-			return 1;
+		if(time == 0) {
+			System.out.println(tab + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
 		} else {
-			return n * factorial(n-1);
+			System.out.println(tab + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+			System.out.println(tab + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+			System.out.println(tab + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+			
+			tab += "____";
+			recur17478(--time, tab);
 		}
 		
+		System.out.println(tabLocal + "라고 답변하였지.");
+		
+		
 	}
+
+	
+//	private static void hanoi(int N, int from, int to, int remain) {
+//		
+//		
+//		if(N == 0) return;
+//		
+//		count++;
+//		
+//		// 바로 전전
+//		hanoi(N-1, from, remain, to);
+//		sb.append(from + " " + to + "\n");
+//		// 바로 전
+//		hanoi(N-1, remain, to, from);
+//	}
+//
+//
+//
+//
+//	private static void starMaker(int x, int y, int N, boolean blank) {
+//		// 공백인 경우
+//		if(blank) {
+//			for(int i = x; i < x + N; i++) {
+//				for(int j = y; j < y + N; j++) {
+//					arr[i][j] = ' ';
+//				}
+//			}
+//			
+//			return;
+//		}
+//		
+//		// 더이상 쪼갤 수 없을 때
+//		if(N == 1) {
+//			arr[x][y] = '*';
+//			
+//			return;
+//		}
+//		
+//		
+//		// 기본
+//		int size = N / 3;
+//		int count = 0;
+//		for (int i = x; i < x + N; i += size) {
+//			for (int j = y; j < y + N; j += size) {
+//				count++;
+//				if (count == 5) { // 공백 칸일 경우
+//					starMaker(i, j, size, true);
+//				} else {
+//					starMaker(i, j, size, false);
+//				}
+//			}
+//		}
+//		
+//		
+//	}
+//
+//
+//	private static int Fibonacci(int n) {
+//		if(n == 0) {
+//			return 0;
+//		} else if(n == 1) {
+//			return 1;
+//		}
+//		
+//		return Fibonacci(n-1) + Fibonacci(n-2);
+//	}
+//
+//
+//	// 재귀 함수
+//	public static int factorial(int n) {
+//		if(n <= 1) {
+//			return 1;
+//		} else {
+//			return n * factorial(n-1);
+//		}
+//		
+//	}
 	
 	
 	
