77170247af322ecc34fa1129da6f496c28b015ea
yooil-405 <yooil405@gmail.com>
2022-05-11 14:21:09 +0900

    20220511 JavaStudy_다형성(Polymorphism)&동적바인딩

--- Java/JavaStudy/src/Polymorphism/Main.java
+++ Java/JavaStudy/src/Polymorphism/Main.java
@@ -1,49 +1,71 @@
 package Polymorphism;
 
-class A {};
+class A {
+	void exe() {
+		System.out.println("A");
+	}
+};
 
-class B extends A {};
+class B extends A {
+	void exe() {
+		System.out.println("B");
+	}
+};
 
-class C extends A {};
+class C extends A {
+	void exe() {
+		System.out.println("C");
+	}
+};
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		A var1 = new A(); // 자신(A)을 인스턴스로 생성
+//		A var1 = new A(); // 자신(A)을 인스턴스로 생성
+//		
+//		// Up Casting
+//		A var2 = new B(); // 자식(B)을 인스턴스로 생성
+//		A var3 = new C(); // 자식(C)을 인스턴스로 생성
+//		
+//		// Down Casting
+//		// var1, var2의 type이 A -> B로 바뀜. (부모에서 자식으로)
+//		B var4 = (B)var2;
+//		B var5 = (B)var1;
+//		
+//		// instanceof 연산자
+//		// 		표현식
+//		/* 		if(레퍼런스 instanceof 클래스타입) {
+//		 * 			// true일때 처리할 내용.
+//		 * 		}
+//		 */
+//		
+//		if(var3 instanceof A) {
+//			System.out.println("O");
+//        } else {
+//        	System.out.println("X");
+//        }
+//		
+//		if(var3 instanceof B) {
+//			System.out.println("O");
+//		} else {
+//			System.out.println("X");
+//		}
+//		
+//		if(var3 instanceof C) {
+//			System.out.println("O");
+//		} else {
+//			System.out.println("X");
+//		}
 		
-		// Up Casting
-		A var2 = new B(); // 자식(B)을 인스턴스로 생성
-		A var3 = new C(); // 자식(C)을 인스턴스로 생성
+		// 동적 바인딩
+		C var1 = new C();
+		var1.exe();
 		
-		// Down Casting
-		// var2의 type이 A -> B로 바뀜. (부모에서 자식으로)
-		B var4 = (B)var2;
+		A var2 = null;
+		var2 = (A)var1;
+		var2.exe();
 		
-		// instanceof 연산자
-		// 		표현식
-		/* 		if(레퍼런스 instanceof 클래스타입) {
-		 * 			// true일때 처리할 내용.
-		 * 		}
-		 */
-		
-		if(var3 instanceof A) {
-			System.out.println("O");
-        } else {
-        	System.out.println("X");
-        }
-		
-		if(var3 instanceof B) {
-			System.out.println("O");
-		} else {
-			System.out.println("X");
-		}
-		
-		if(var3 instanceof C) {
-			System.out.println("O");
-		} else {
-			System.out.println("X");
-		}
 	}
 
 }
