274ef6767f8b22d93affb7b43408e06edaee2ddf
yooil-405 <yooil405@gmail.com>
2022-05-13 23:06:35 +0900

    20220513 백준문제풀이_16173, 2667(BFS, DFS)

--- codingTest/CodingTest/src/main/java/com/study/yooil/BAEKJOON/DFS_BFS/Main.java
+++ codingTest/CodingTest/src/main/java/com/study/yooil/BAEKJOON/DFS_BFS/Main.java
@@ -12,12 +12,18 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static boolean[] isVisit;
+//	static boolean[] isVisit;
 	static boolean[][] isVisit2;
-	static ArrayList<Integer>[] line;
-	static StringBuilder sb;
-	static int[][] apt;
-	static List<Integer> result;
+//	static ArrayList<Integer>[] line;
+//	static StringBuilder sb;
+//	static int[][] apt;
+//	static List<Integer> result;
+//	static int[][] square;
+	static int[][] map;
+	static int hNum;
+	static int dx[];
+	static int dy[];
+	static int N;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -96,71 +102,202 @@ public class Main {
 //		
 //		System.out.println(bfs());
 		
-		// 2667
-		int N = 0;
+//		// 2667
+//		int N = 0;
+//		
+//		N = Integer.parseInt(br.readLine());
+//		
+//		apt = new int[N][N];
+//		isVisit2 = new boolean[N][N];
+//		
+//		int i = 0;
+//		while(N-- > 0) {
+//			String floor = br.readLine();
+//			
+//			for(int j = 0; j < floor.length(); j++) {
+//				apt[i][j] = floor.charAt(j) - 48;
+//			}
+//			
+//			i++;
+//		}
+//		
+//		// 입력 확인 출력
+////		for(int j = 0; j < apt.length; j++) {
+////			for(int k = 0; k < apt[j].length; k++) {
+////				System.out.print(apt[j][k]);
+////			}
+////			System.out.println();
+////		}
+//		
+//		StringBuilder sb = new StringBuilder();
+//		result = new ArrayList<>();
+//		
+//		dfs();
+		
+//		// 16173 점프왕 쪨리(Small)
+//		int N = Integer.parseInt(br.readLine());
+//		
+//		square = new int[N][N];
+//		isVisit2 = new boolean[N][N];
+//		
+//		for(int i = 0; i < N; i++) {//			
+//			StringTokenizer st = new StringTokenizer(br.readLine());//	
+		
+//			for(int j = 0; j < N; j++) {//				
+//				square[i][j] = Integer.parseInt(st.nextToken());
+//			}
+//		}
+//		
+////		for (int[] is : square) {
+////			for (int i : is) {
+////				System.out.print(i + " ");
+////			}
+////			
+////			System.out.println();
+////		}
+//		
+//		boolean result = bfs(0, 0 , N);
+//		
+//		if(result) {
+//			System.out.println("HaruHaru");
+//		} else {
+//			System.out.println("Hing");
+//		}
+		
+		// 2667 단지번호붙이기
+		N = 0;
 		
 		N = Integer.parseInt(br.readLine());
 		
-		apt = new int[N][N];
+		map = new int[N][N];
 		isVisit2 = new boolean[N][N];
+		dx = new int[] {0, 0, 1, -1};
+		dy = new int[] {1, -1, 0, 0};
 		
-		int i = 0;
-		while(N-- > 0) {
-			String floor = br.readLine();
+		for(int i = 0; i < N; i++) {			
+			String S = br.readLine();
 			
-			for(int j = 0; j < floor.length(); j++) {
-				apt[i][j] = floor.charAt(j) - 48;
+			for(int j = 0; j < N; j++) {				
+				map[i][j] = S.charAt(j) - 48;
 			}
-			
-			i++;
 		}
 		
-		// 입력 확인 출력
-//		for(int j = 0; j < apt.length; j++) {
-//			for(int k = 0; k < apt[j].length; k++) {
-//				System.out.print(apt[j][k]);
+		int cNum = 0; // 단지수
+		List<Integer> aptNum = new ArrayList<Integer>();
+		
+		for(int i = 0; i < N; i++) {			
+			for(int j = 0; j < N; j++) {	
+				if(map[i][j] == 1 && !isVisit2[i][j]) {
+					hNum = 0;					
+					dfs(i, j);
+					
+					aptNum.add(hNum);
+					
+					cNum++;
+				}
+			}
+		}
+		
+		Collections.sort(aptNum);
+		
+		
+		System.out.println(cNum);
+		
+		
+		for (Integer i : aptNum) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	// 2667
+	private static void dfs(int r, int c) {
+		hNum++;
+		 // 단지 내 집의 수
+		isVisit2[r][c] = true;
+		
+		for(int i = 0; i < 4; i++) {
+			int tempR = r + dx[i];
+			int tempC = c + dy[i];
+			
+			if(tempR >= 0  && tempR < N && tempC >= 0  && tempC < N) {
+				if(map[tempR][tempC] == 1 && !isVisit2[tempR][tempC]) {
+					dfs(tempR, tempC);
+					
+				}
+			}
+		}
+	}
+	
+	
+//	// 16173
+//	private static boolean bfs(int row, int column, int len) {
+//		Queue<int[]> q = new LinkedList<>();
+//		q.offer(new int[] {row, column});
+//		
+//		boolean success = false;
+//		
+//		while(!q.isEmpty()) {
+//			int[] temp = q.poll();
+//			int r = temp[0];
+//			int c = temp[1];
+//			
+//			isVisit2[row][column] = true;
+//			int move = square[r][c];
+//			
+//			if(square[r][c] == -1) {
+//				success = true;
+//				
+//				break;
 //			}
-//			System.out.println();
+//			
+//			int newR = r + move;
+//			int newC = c + move;
+//			
+//			if(newR < len && !isVisit2[newR][c]) {
+//				q.offer(new int[] {newR, c});
+//				
+//				isVisit2[newR][c] = true;
+//			}
+//			if(newC < len && !isVisit2[r][newC]) {
+//				q.offer(new int[] {r, newC});
+//				
+//				isVisit2[r][newC] = true;
+//			}
+//			
+//			
+//			
 //		}
-		
-		StringBuilder sb = new StringBuilder();
-		result = new ArrayList<>();
-		
-		dfs();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+//		
+//		
+//		return success;
+//	}
 	
 	
-	
-	
-	
-	
-private static void dfs() {
-	int count = 0;
-	
-	for(int i = 0; i < apt.length; i++) {
-		for(int j = 0; j < apt[i].length; j++) {
-			if(isVisit2[i][j] == false && apt[i][j] == 1) {
-				isVisit2[i][j] = true;
-				
-			}
-		}
-	}
-		
-	}
+//	private static void dfs() {
+//		int count = 0;
+//		
+//		for(int i = 0; i < apt.length; i++) {
+//			for(int j = 0; j < apt[i].length; j++) {
+//				if(isVisit2[i][j] == false && apt[i][j] == 1) {
+//					isVisit2[i][j] = true;
+//				}
+//			}
+//		}
+//	}
 
 
 
@@ -176,29 +313,29 @@ private static void dfs() {
 	
 
 
-	private static int bfs() {
-		Queue<Integer> q = new LinkedList<>();
-		isVisit[1] = true;
-		int count = 0;
-		
-		q.offer(1);
-		
-		while(!q.isEmpty()) {
-			int x = q.poll();
-			for(int i = 0; i < line[x].size(); i++) {
-				int k = line[x].get(i);
-				
-				if(isVisit[k] == false) {
-					q.offer(k);
-					isVisit[k] = true;
-					count++;
-				}
-				
-			}
-		}
-		
-		return count;
-	}
+//	private static int bfs() {
+//		Queue<Integer> q = new LinkedList<>();
+//		isVisit[1] = true;
+//		int count = 0;
+//		
+//		q.offer(1);
+//		
+//		while(!q.isEmpty()) {
+//			int x = q.poll();
+//			for(int i = 0; i < line[x].size(); i++) {
+//				int k = line[x].get(i);
+//				
+//				if(isVisit[k] == false) {
+//					q.offer(k);
+//					isVisit[k] = true;
+//					count++;
+//				}
+//				
+//			}
+//		}
+//		
+//		return count;
+//	}
 
 
 
