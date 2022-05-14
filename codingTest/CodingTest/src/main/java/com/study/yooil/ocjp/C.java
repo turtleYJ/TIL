7c3a3b57edacb16a26a23b7213f325cc11cad62b
yooil-405 <yooil405@gmail.com>
2022-05-14 10:24:35 +0900

    20220514 ocjp 관련 파일 이동

--- codingTest/CodingTest/src/main/java/com/study/yooil/ocjp/C.java
+++
@@ -1,9 +0,0 @@
-package com.study.yooil.ocjp;
-
-import java.io.IOException;
-
-class A {
-	public void main(String fileName) throws IOException {
-		
-	}
-}
--- codingTest/CodingTest/src/main/java/com/study/yooil/ocjp/test/OraString.java
+++
@@ -1,8 +0,0 @@
-package com.study.yooil.ocjp.test;
-
-public class OraString {
-	String s;
-	public boolean equals(OraString str) {
-		return this.s.equalsIgnoreCase(str.toString());
-	}
-}
