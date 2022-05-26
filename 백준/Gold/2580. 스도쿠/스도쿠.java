de2f4f540c3890bb145755314c67e2c81b01826d
YooilJung <yooil405@gmail.com>
2022-05-26 18:10:14 +0900

    [Gold IV] Title: 스도쿠, Time: 500 ms, Memory: 25956 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static int N;
+	private static int[][] board;
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		board = new int[9][9];
+		
+		
+		for(int i = 0; i < 9; i++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for(int j = 0; j < 9; j++) {
+				
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		sudoku(0, 0);
+	}
+
+	private static void sudoku(int row, int col) {
+		if(col == 9) {
+			sudoku(row + 1, 0);
+			
+			return;
+		}
+		
+		if(row == 9) {
+			for (int[] is : board) {
+				
+				for (int i : is) {
+					
+					System.out.print(i + " ");
+				}
+				
+				System.out.println();
+			}
+			
+			System.exit(0);
+		}
+		
+		if(board[row][col] == 0) {
+			for(int i = 0; i < 9; i++) {
+				
+				if(isPossible(row, col, i + 1)) {
+					board[row][col] = i + 1;
+					
+					sudoku(row, col + 1);
+				}
+			}
+			
+			board[row][col] = 0;
+			return;
+		}
+		
+		sudoku(row, col + 1);
+	}
+
+	private static boolean isPossible(int row, int col, int value) {
+		// 행, 열 검사
+		for(int i = 0; i < 9; i++) {
+			
+			if(board[row][i] ==  value) {
+				
+				return false;
+			}
+			
+			if(board[i][col] ==  value) {
+				
+				return false;
+			}
+		}
+		
+		int r = row / 3 * 3;
+		int c = col / 3 * 3;
+		
+		for(int i = r; i < r + 3; i++) {
+			
+			for(int j = c; j < c + 3; j++) {
+				
+				if(board[i][j] ==  value) {
+					
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+}
