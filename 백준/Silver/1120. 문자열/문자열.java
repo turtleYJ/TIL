76e5d6abaa49d07d4237127b4419a69c574c730e
YooilJung <yooil405@gmail.com>
2022-05-27 22:20:35 +0900

    Update 문자열.java

--- 백준/Silver/1120. 문자열/문자열.java
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 문자열 매칭문제
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
