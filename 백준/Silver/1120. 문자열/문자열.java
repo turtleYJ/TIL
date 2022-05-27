abf336a3f9ce20e31d7f60b9f8d213a21a9b1f33
YooilJung <yooil405@gmail.com>
2022-05-27 22:01:59 +0900

    [Silver IV] Title: 문자열, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        String A = st.nextToken();
+        String B = st.nextToken();
+        
+        int lengthDiff = B.length() - A.length();
+        
+        int minCount = Integer.MAX_VALUE;
+        
+        for(int i = 0; i <= lengthDiff; i++) {
+        	
+        	int count = 0;
+        	
+        	for(int j = 0; j < A.length(); j++) {
+        		
+        		if(A.charAt(j) != B.charAt(j + i)) {
+        			count++;
+        		}
+        	}
+        	
+        	minCount = Math.min(minCount, count);
+        }
+        
+        System.out.println(minCount);
+    }
+}
