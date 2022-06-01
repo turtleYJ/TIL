7240658d7ebc4612b106cd55f940834ca0e5fccf
YooilJung <yooil405@gmail.com>
2022-06-01 20:07:44 +0900

    [Silver II] Title: 최대 힙, Time: 324 ms, Memory: 26304 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+// 정렬된 map
+// TreeMap
+// comparator사용
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num == 0) {
+				if(pq.size() == 0) {
+					
+					sb.append(0).append('\n');
+				} else {
+					
+					sb.append(pq.poll()).append('\n');
+				}
+			} else {
+				
+				pq.offer(num);
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
