bf672a588ac8c1b38ef0e9524e5c9a1551399464
yooil-405 <yooil405@gmail.com>
2022-06-02 14:32:34 +0900

    20220602 자바의정석_ch6문제풀이

---
+++ Java/자바의정석/ch6/연습문제/Exercise6_1.java
@@ -0,0 +1,6 @@
+
+class SutdaCard {
+    int num;
+    boolean isKwang;
+
+}
---
+++ Java/자바의정석/ch6/연습문제/Exercise6_3.java
@@ -0,0 +1,8 @@
+class Student {
+    String name; // 학생이름
+    int ban; // 반
+    int no; //번호
+    int kor; //국어점수
+    int eng; //영어점수
+    int math; //수학점수
+}
