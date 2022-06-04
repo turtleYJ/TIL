5e156c705bb1f3914673b78573c424cb686df733
YooilJung <yooil405@gmail.com>
2022-06-04 17:49:51 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 396 ms, Memory: 27656 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+    	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean arr[] = new boolean[10000001];
+        
+        arr[0] = true;
+		arr[1] = true;
+        
+		// 에라토스테네스의 체
+        for(int i = 2; i <= Math.sqrt(10000001); i++) {
+			// 아래에서 i의 제곱수 부터 검사를 시작함으로 전체범위의 제곱근까지 검사함.
+			for(int j = i*i; j < 10000001; j += i) {
+				arr[j] = true;
+			}
+		}
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = N; i < 10000001; i++) {
+        	
+        	if(!arr[i] && isPalindrome(i)) {
+        		System.out.println(i);
+        		return;
+        	}
+		}
+    }
+
+	private static boolean isPalindrome(int value) {
+		String S = Integer.toString(value);
+    	
+    	for (int j = 0; j < S.length() / 2; j++) {
+			if(S.charAt(j) != S.charAt(S.length() - 1 - j)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
