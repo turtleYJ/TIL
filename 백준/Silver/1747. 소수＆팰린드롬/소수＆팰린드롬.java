8d1012df7eaea367632e4d6726d6ab2671b85b7e
YooilJung <yooil405@gmail.com>
2022-06-04 21:34:32 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 368 ms, Memory: 18224 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -8,30 +8,26 @@ public class Main {
     	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        boolean arr[] = new boolean[10000001];
-        
-        arr[0] = true;
-		arr[1] = true;
-        
-		// 에라토스테네스의 체
-        for(int i = 2; i <= Math.sqrt(10000001); i++) {
-			// 아래에서 i의 제곱수 부터 검사를 시작함으로 전체범위의 제곱근까지 검사함.
-			for(int j = i*i; j < 10000001; j += i) {
-				arr[j] = true;
-			}
-		}
-        
         int N = Integer.parseInt(br.readLine());
+        int i = N;
         
-        for (int i = N; i < 10000001; i++) {
-        	
-        	if(!arr[i] && isPalindrome(i)) {
+        while(true) {
+        	if(isPrime(i) && isPalindrome(i)) {
         		System.out.println(i);
         		return;
         	}
-		}
+        	i++;
+        }
     }
-	// 팰린드롬 수인지 판별
+
+	private static boolean isPrime(int n) {
+		if(n == 0 || n == 1) return false;
+		for(int i = 2; i <= (int)Math.sqrt(n); i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+	}
+
 	private static boolean isPalindrome(int value) {
 		String S = Integer.toString(value);
     	
