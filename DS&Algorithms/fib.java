2e77b60159c54df97a4866c3ebc7b31f1f86fabb
yooil-405 <yooil405@gmail.com>
2022-06-08 12:29:48 +0900

    20220608 Dynamic Programming study 1

---
+++ DS&Algorithms/fib.java
@@ -0,0 +1,11 @@
+// memoization
+// Dynamic Programming
+// fibonacci
+public class Fib {
+    long fibo(int n, HashMap<Integer, Long> memo) {
+    	if (memo.containsKey(n)) return memo.get(n);
+    	if (n <= 2) return 1;
+    	memo.put(n, fibo(n - 1, memo) + fibo(n - 2, memo));
+    	return memo.get(n);
+    }
+}
