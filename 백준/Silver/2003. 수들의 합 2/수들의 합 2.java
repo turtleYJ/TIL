d7a0ac40669b30a10e5e472e5b4e5187d6013df5
Yooil Jung <yooil405@gmail.com>
2022-06-24 21:37:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 14964 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -13,30 +13,47 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
+		int[] data = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < data.length; i++) {
+			data[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int start = 0;
+//		int start = 0;
 		int end = 0;
-		int sum = 0;
-		int res = 0;
-		while(start < arr.length) {
-			// end가 끝에 도달하면 start를 강제로 올려줘서 와일문을 끝내준다.
-			if (sum >= M || end == N) {
-				if(sum == M) res++;
-				sum -= arr[start];
-				start++;
-			}
-			
-			else {
-				sum += arr[end];
+//		int sum = 0;
+		int summary = 0;
+//		int res = 0;
+		int result = 0;
+//		while(start < arr.length) {
+//			// end가 끝에 도달하면 start를 강제로 올려줘서 와일문을 끝내준다.
+//			if (sum >= M || end == N) {
+//				if(sum == M) res++;
+//				sum -= arr[start];
+//				start++;
+//			}
+//			
+//			else {
+//				sum += arr[end];
+//				end++;
+//			}
+//		}
+//		System.out.println(res);
+		
+		for(int start = 0; start <N; start++) {
+			// end를 가능한 만큼 이동시키기
+			while(summary < M && end < N) {
+				summary += data[end];
 				end++;
 			}
+			// 부분합이 M일때 카운트 증가
+			if(summary == M) {
+				result++;
+			}
+			summary -= data[start];
 		}
-		System.out.println(res);
+		
+		System.out.println(result);
 	}
 }
