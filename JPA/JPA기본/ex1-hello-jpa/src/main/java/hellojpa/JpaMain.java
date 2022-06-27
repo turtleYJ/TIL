aaa693ddb7e2458d3c44b86978811ff02f96297c
tutleJ <yooil405@gmail.com>
2022-06-27 19:58:36 +0900

    20220627 JPA 실습

--- JPA/JPA기본/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/JPA기본/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,8 +21,24 @@ public class JpaMain {
 
         // code
         try {
+            Member member1 = new Member();
+            member1.setUsername("A");
+            Member member2 = new Member();
+            member2.setUsername("B");
+            Member member3 = new Member();
+            member3.setUsername("C");
 
+            System.out.println("=========================");
 
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
+
+            System.out.println("=========================");
 
             tx.commit();
         } catch (Exception e) {
--- JPA/JPA기본/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA/JPA기본/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,31 +4,37 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-//테이블 명이 클래스명과 다를 때
-//@Table(name = "USER")
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
-    private long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+        generator =  "MEMBER_SEQ_GENERATOR")
+    private Long id;
 
     @Column(name = "name")
     private String username;
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
     public Member() {
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
