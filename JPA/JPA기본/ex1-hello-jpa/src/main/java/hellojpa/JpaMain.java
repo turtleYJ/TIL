1a644ee77fee9f54c4e7436e41b92440e31d8652
tutleJ <yooil405@gmail.com>
2022-06-30 22:22:09 +0900

    20220630 JPA 실습

--- JPA/JPA기본/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/JPA기본/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -33,8 +33,11 @@ public class JpaMain {
 
             Member findMember = em.find(Member.class, member.getId());
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m: members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
