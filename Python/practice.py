a022150cd6140a9a2232b677dd026cfe3bc31b93
tutleJ <yooil405@gmail.com>
2022-07-04 14:54:48 +0900

    20220704 python study

---
+++ Python/practice.py
@@ -0,0 +1,91 @@
+# 숫자
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# 문자열
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋ")
+# print('ㅋ'*9)
+
+# 참 / 거짓
+# print(5 >10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not (5 > 10))
+
+# 애완동물을 소개해 주세요~
+# animal = "강아지"
+# name = "연탄이"
+# age = 4
+# hobby = "산책"
+# is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + "예요")
+# # print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
+'''
+여러 문장을 주석 처리할 때
+'''
+
+# Quiz)
+# station = "사당"
+
+# print(station, "행 열차가 들어오고 있습니다.")
+
+# 연산자
+# print(1 + 1)
+# print(3 - 2)
+# print(5 * 2)
+# print(6 / 3)
+
+# print(2**3) # 두번 곱하면 제곱
+# print(5%3)
+# print(10%3)
+# print(10//3) # 두번 나누면 몫
+# print(5//3)
+
+# print(10 > 3)
+# print(4 >= 7)
+# print(10 < 3)
+# print(5 <= 5)
+
+# print(3 == 3)
+# print(4 == 2)
+# print(3 + 4 == 7)
+
+# print(1 != 3)
+# print(not(1 != 3))
+
+# print((3 > 0) and (3 < 5))
+# print((3 > 0) & (3 < 5))
+
+# print((3 > 0) or  (3 > 5))
+# print((3 > 0) | (3 > 5))
+
+# print(5 > 4 > 3)
+# print(5 > 4 > 7)
+
+# 수식
+
+
+
+
+
+
+
+
+
+
+
+
