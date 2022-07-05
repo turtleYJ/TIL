e22d8e379a306bcfbd5c31f3371e93692c6859c5
tutleJ <yooil405@gmail.com>
2022-07-05 16:27:39 +0900

    20220705 JPA 실습_연관관계 매핑 예제

--- JPA/JPA기본/jpashop/src/main/java/jpabook/jpashop/jpaMain.java
+++ JPA/JPA기본/jpashop/src/main/java/jpabook/jpashop/jpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -20,6 +23,8 @@ public class jpaMain {
         // code
         try {
 
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
 
             tx.commit();
         } catch (Exception e) {
