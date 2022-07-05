dc3527dc95b177fdf23a2b0ce6104960777e7718
tutleJ <yooil405@gmail.com>
2022-07-05 14:30:54 +0900

    20220705 python study

--- Python/practice.py
+++ Python/practice.py
@@ -77,6 +77,157 @@
 # print(5 > 4 > 7)
 
 # 수식
+# print(2 + 3 * 4)
+# print((2 + 3) * 4)
+# number = 2 + 3 * 4
+# print(number)
+# number = number + 2
+# print(number)
+# number += 2
+# print(number)
+# number *= 2
+# print(number)
+# number /= 2
+# print(number)
+# number -= 2
+# print(number
+
+# number %= 5
+# print(number)
+
+# 숫자처리 함수
+# print(abs(-5))
+# print(pow(4, 2))
+# print(max(5, 12))
+# print(min(5, 12))
+# print(round(3.14))
+# print(round(4.99))
+# print("==========")
+
+# from math import *
+# print(floor(4.99))
+# print(ceil(3.14))
+# print(sqrt(16))
+
+# 랜덤 함수
+# from random import *
+
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10) # 0,0 ~ 10.0 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+
+# Quiz) 코딩 스터디 모임 날짜 정하기
+# 매월 4 ~ 28 중 하루를 선정하기
+# from random import *
+
+# date = randrange(3, 29)
+# print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다.")
+
+# 문자열
+# sentence = '나는 소년입니다.'
+# print(sentence)
+# sentence2 = "나는 소년입니다."
+# print(sentence2)
+# sentence3 = """
+# 나는 소년이고,
+# 파이썬은 쉬워요
+# """
+# print(sentence3)
+
+# # 슬라이싱
+# jumin = "990120-1234567"
+
+# print("성별 : " + jumin[7])
+# print("연 : " + jumin[0:2])
+# print("월 : " + jumin[2:4])
+# print("일 : " + jumin[4:6])
+# # print("생년월일 : " + jumin[0:6])
+# print("생년월일 : " + jumin[:6]) # 처음부터 6 직전까지 
+# print("뒤 7자리 : " + jumin[7:]) # 7부터 끝까지 
+# print("뒤 7자리 (뒤에서부터) : " + jumin[-7:])
+# # 맨 뒤에서 7번째부터 끝까지
+
+# # 문자열 처리
+# python = "Python is Amazing"
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper())
+# print(len(python))
+# print(python.replace("Python", "Java"))
+
+# index = python.index("n")
+# print(index)
+# index = python.index("n", index + 1)
+# print(index)
+
+# print(python.find("Java"))
+# # print(python.index("Java"))
+
+# print(python.count("n"))
+
+
+# 문자열 포맷
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
