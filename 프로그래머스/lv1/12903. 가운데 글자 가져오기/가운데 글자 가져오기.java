a657c2bc298b30dc4f4364f9d3820eb6cff0df5b
Yooil Jung <yooil405@gmail.com>
2022-07-21 23:46:56 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.07 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+        
+        if (len % 2 == 1) {
+        	answer += s.charAt(len / 2);
+        } else {
+        	answer += s.charAt(len / 2 - 1);
+        	answer += s.charAt(len / 2);
+        }
+        
+        return answer;
+    }
+}
