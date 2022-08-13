caaacfcd4a55854e3c92fd7afaf8645ce036e461
yooil-405 <yooil405@gmail.com>
2022-08-13 15:20:44 +0900

    20220813 REST API study_버전관리법

--- REST API/restful-web-service/src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ REST API/restful-web-service/src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -37,9 +38,11 @@ public class AdminUserController {
         return mapping;
     }
 
-    // GET /users/1 or /users/10
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+    // GET /admin/users/1 -> /admin/v1/users/1
+//    @GetMapping("/v1/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=1")
+    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
@@ -52,6 +55,31 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+//    @GetMapping("/v2/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=2")
+    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filters);
 
         return mapping;
--- REST API/restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ REST API/restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 //@JsonIgnoreProperties(value = {"password"})
+@NoArgsConstructor
 @JsonFilter("UserInfo")
 public class User {
     private Integer id;
