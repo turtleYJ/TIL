03cd5b1a7de80d9831bf54edb2a4f8ba276b3396
turtleYJ <yooil405@gmail.com>
2022-11-08 14:13:51 +0900

    1108 PHP_Grammar_Array

--- php/Grammar/Types/Array.php
+++ php/Grammar/Types/Array.php
@@ -57,3 +57,94 @@ $array = array(
 );
 var_dump($array);
 ?>
+
+# Example #6 Complex Type Casting and Overwriting example
+<?php
+$array = array(
+    1       =>      'a',
+    '1'     =>      'b',
+    1.5     =>      'c',
+    -1      =>      'd',
+    '01'    =>      'e',
+    '1.5'   =>      'f',
+    true    =>      'g',
+    false   =>      'h',
+    ''      =>      'i',
+    null    =>      'j',
+    'k',
+    2       =>      'l',
+);
+var_dump($array);
+?>
+
+# Example #7 Accessing array elements
+<?php
+$array = array(
+    "foo"   =>  'bar',
+    42      =>  24,
+    "multi" => array(
+        "dimensional"   => array(
+            "array"     =>  "foo"
+        )
+    )
+);
+var_dump($array["foo"]);
+var_dump($array[42]);
+var_dump($array["multi"]["dimensional"]["array"]);
+?>
+
+# Example #8 Array dereferencing
+<?php
+function getArray() {
+    return array(1, 2, 3);
+}
+
+var_dump(getArray()[1]);
+?>
+
+<?php
+$arr = array(5 => 1, 12 => 2);
+
+$arr[] = 56; // This is the same as $arr[13] = 56;
+             // at this point of the script
+
+$arr["x"] = 42; // This adds a new element to
+                // the array with key "x"
+
+unset($arr[5]); // This removes the element from the array
+unset($arr); // This deletes the whole array
+?>
+
+<?php
+$array = array(1, 2, 3, 4, 5);
+print_r($array);
+
+foreach ($array as $i => $value) {
+    unset($array[$i]);
+}
+print_r($array);
+
+// Append an item (note that the new key is 5, instead of 0)
+$array[] = 6;
+print_r($array);
+
+// Re-index: (array_values)
+$array = array_values($array);
+print_r($array);
+$array[] = 7;
+print_r($array);
+?>
+
+<?php
+$source_array = ['foo', 'bar', 'baz'];
+print_r($source_array);
+
+[$foo, $bar, $baz] =  $source_array;
+print_r($source_array);
+
+echo $foo;
+echo $bar;
+echo $baz;
+?>
+
+>> 이후에 필요하면 더 실습할 것.
