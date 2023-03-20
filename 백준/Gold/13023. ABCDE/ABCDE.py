bfa3840af6b50211512f6d6d1b30a3a9a5bb4123
Yooil Jung <yooil405@gmail.com>
2023-03-20 21:54:42 +0900

    [Gold V] Title: ABCDE, Time: 752 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,30 +1,26 @@
-import sys
-
-# Set recursion limit
-sys.setrecursionlimit(10000)
-
-def dfs(node, length):
-    visited[node] = True
-    if length == 4:
-        print(1)
-        exit()
-    for neighbor in adj_list[node]:
-        if not visited[neighbor]:
-            dfs(neighbor, length+1)
-    visited[node] = False
-
-# Read input
+def dfs(graph, start, visited, cnt):
+  visited[start] = True
+  if cnt == 4:
+    print('1')
+    exit() 
+  for ne in graph[start]:
+    if not visited[ne]:
+      dfs(graph, ne, visited, cnt+1)
+  visited[start] = False    
+  
 n, m = map(int, input().split())
+
+# 인접 리스트 그래프 구현
 adj_list = [[] for _ in range(n)]
 for _ in range(m):
-    a, b = map(int, input().split())
-    adj_list[a].append(b)
-    adj_list[b].append(a)
+  a, b = map(int, input().split())
+  adj_list[a].append(b)
+  adj_list[b].append(a)
 
-# Perform DFS
-visited = [False] * n
+visited = [False] * (n)
+
 for i in range(n):
-    dfs(i, 0)
+  if not visited[i]:
+    dfs(adj_list, i, visited, cnt = 0)
 
-# If no path of length 5 was found, output 0
-print(0)
+print('0')
