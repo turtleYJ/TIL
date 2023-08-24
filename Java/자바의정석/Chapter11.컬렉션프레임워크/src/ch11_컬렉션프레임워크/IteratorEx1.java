875d9a0dd7d2f8fabdcf84feba81731d15a4a8e2
turtleYJ <yooil405@gmail.com>
2023-08-24 20:22:39 +0900

    20230824 java's standard ch11 문제풀이_Arrays

--- Java/자바의정석/Chapter11.컬렉션프레임워크/src/ch11_컬렉션프레임워크/IteratorEx1.java
+++ Java/자바의정석/Chapter11.컬렉션프레임워크/src/ch11_컬렉션프레임워크/IteratorEx1.java
@@ -15,6 +15,12 @@ public class IteratorEx1 {
 		
 		Iterator it = list.iterator();
 		
-		while (it.hasNext()) System.out.println(it.next());
+		while (it.hasNext()) {
+			System.out.println(it.next());
+			// 넥스트 이후에 리무브를 하면 해당 값이 삭제된다.(추출 후 삭제)
+//			it.remove();
+		}
+		
+		System.out.println(list);
 	}
 }
