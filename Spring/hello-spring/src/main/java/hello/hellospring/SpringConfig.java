d1721840f177d19bb3b7891f18bef88f51a06201
turtleYJ <yooil405@gmail.com>
2023-11-05 22:55:46 +0900

    20231105 Spring Project creation

---
+++ Spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,35 @@
+package hello.hellospring;
+
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private EntityManager em;
+
+    // 생성자를 통한 의존성 주입
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemeberRepository(em);
+    }
+
+}
