7a29b5d947f3f2618ecf4aa8172f3f4b39d42d73
turtleYJ <yooil405@gmail.com>
2023-11-06 23:04:03 +0900

    20231106 CI4 tutorial day1 _ static pages, pages conteroller

---
+++ php&CodeIgniter/CI4/CI4_temp/app/Controllers/Pages.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home')
+    {
+        if (! is_file(APPPATH . 'Views/pages' . $page . '.php')) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page);
+
+        return view('templates/heaser', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
+    }
+}
