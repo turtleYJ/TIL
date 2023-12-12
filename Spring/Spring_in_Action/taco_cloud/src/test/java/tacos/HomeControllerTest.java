40355139331ed509dff8ff3849239378a87c465b
turtleYJ <yooil405@gmail.com>
2023-12-12 22:01:15 +0900

    20231211 Spring Taco project _ 뷰컨트롤러 추가

--- Spring/Spring_in_Action/taco_cloud/src/test/java/tacos/HomeControllerTest.java
+++ Spring/Spring_in_Action/taco_cloud/src/test/java/tacos/HomeControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 public class HomeControllerTest {
 	
 	@Autowired
