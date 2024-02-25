b417ca879372e19dde38dc1a598845f09b606c24
turtleYJ <yooil405@gmail.com>
2024-02-25 21:55:50 +0900

    상품-주문 API TDD로 개발하기
    * 스프링부트 테스트로 전환

--- TDD/product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ TDD/product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -2,19 +2,14 @@ package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private ProductPort productPort;
-    private ProductRepository productReposity;
-
-    @BeforeEach
-    void setUp() {
-        productReposity = new ProductRepository();
-        productPort = new ProductAdapter(productReposity);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     void 상품등록() {
