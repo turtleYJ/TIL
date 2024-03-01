0cb647813cee44d0e1eab8a9fe6554297169b2f6
turtleYJ <yooil405@gmail.com>
2024-03-02 08:12:14 +0900

    상품-주문 API TDD로 개발하기
    * API 테스트로 전환

--- TDD/product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
+++ TDD/product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
@@ -1,8 +1,15 @@
 package com.example.productorderservice.product;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
+@RequestMapping("/products")
 class ProductService {
     private final ProductPort productPort;
 
@@ -10,9 +17,11 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    public void addProduct(AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity addProduct(@RequestBody AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
-
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- TDD/product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ TDD/product-order-service/src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -6,23 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductServiceTest {
+public class ProductServiceTest {
 
     @Autowired
     private ProductService productService;
-
     @Test
     void 상품등록() {
-        final AddProductRequest request = 상품등록요청_생성();
-
+        final AddProductRequest request = 상품등록_요청();
         productService.addProduct(request);
     }
 
-    private static AddProductRequest 상품등록요청_생성() {
-        final String name = "상품영";
+    private static AddProductRequest 상품등록_요청() {
+        final String name = "상품명";
         final int price = 1000;
         final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
+        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy.NONE);
         return request;
     }
+
 }
