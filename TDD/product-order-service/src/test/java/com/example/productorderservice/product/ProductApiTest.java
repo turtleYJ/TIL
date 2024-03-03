1d5a7bc6276791b660798632c8cff85207e0cd61
turtleYJ <yooil405@gmail.com>
2024-03-03 21:48:19 +0900

    상품-주문 API TDD로 개발하기
    * POJO 상품 수정 기능 구현

--- TDD/product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ TDD/product-order-service/src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -1,13 +1,8 @@
 package com.example.productorderservice.product;
 
 import com.example.productorderservice.ApiTest;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -28,12 +23,9 @@ class ProductApiTest extends ApiTest {
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
         Long productId = 1L;
 
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .get("/products/{productId}", productId)
-                .then().log().all()
-                .extract();
+        final var response = ProductSteps.상품조회요청(productId);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
+
 }
