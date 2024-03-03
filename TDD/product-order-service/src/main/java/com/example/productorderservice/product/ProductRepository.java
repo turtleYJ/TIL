a6e73373171464cd11edb838065ec105f11989b5
turtleYJ <yooil405@gmail.com>
2024-03-03 11:53:37 +0900

    상품-주문 API TDD로 개발하기
    * JPA로 전환
    
    * databaseCleanup 도입하여 테스트 격리

--- TDD/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ TDD/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -1,17 +1,15 @@
 package com.example.productorderservice.product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Repository
-class ProductRepository {
-    private Long sequence = 0L;
-    private Map<Long, Product> persistence = new HashMap<>();
-
-    public void save(Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
+interface ProductRepository extends JpaRepository<Product, Long> {
 }
