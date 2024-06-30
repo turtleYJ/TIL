03511fafe3075e6e088eaea57a9e9e5fd3e9a0c2
turtleYJ <yooil405@gmail.com>
2024-06-30 17:17:23 +0900

    feat(TDD): 중복 제거 (부모 클래스 생성)

--- TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/UnitTest.java
+++ TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/UnitTest.java
@@ -8,10 +8,15 @@ public class UnitTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 
     @Test
@@ -19,11 +24,12 @@ public class UnitTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
-    private class Dollar {
-        public int amount;
-
+    private class Dollar extends Money {
         public Dollar(int amount) {
             this.amount = amount;
         }
@@ -31,10 +37,24 @@ public class UnitTest {
         public Dollar times(int multiplier) {
             return new Dollar(amount * multiplier);
         }
+    }
+
+    private class Franc extends Money {
+        public Franc(int amount) {
+            this.amount = amount;
+        }
+
+        public Franc times(int multiplier) {
+            return new Franc(amount * multiplier);
+        }
+    }
+
+    class Money {
+        protected int amount;
 
         public boolean equals(Object object) {
-            Dollar dollar = (Dollar) object;
-            return amount == dollar.amount;
+            Money money = (Money) object;
+            return amount == money.amount && getClass().equals(money.getClass());
         }
     }
 }
