f8fd0baa0b4d2a4ff6f75ade70af78bb6ef74075
turtleYJ <yooil405@gmail.com>
2024-06-30 16:33:01 +0900

    feat(TDD): equals 구현

--- TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/UnitTest.java
+++ TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/UnitTest.java
@@ -2,14 +2,23 @@ package com.example.my_tdd_project;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+//  삼각 측량
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
     private class Dollar {
@@ -19,8 +28,13 @@ public class UnitTest {
             this.amount = amount;
         }
 
-        public void times(int multiplier) {
-            amount *= multiplier;
+        public Dollar times(int multiplier) {
+            return new Dollar(amount * multiplier);
+        }
+
+        public boolean equals(Object object) {
+            Dollar dollar = (Dollar) object;
+            return amount == dollar.amount;
         }
     }
 }
