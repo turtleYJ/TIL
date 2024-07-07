c05f20414c7f91f22fb94a4d8692faaf5f714b6a
turtleYJ <yooil405@gmail.com>
2024-07-07 23:23:52 +0900

    feat(TDD): 15장 서로 다른 통화 더하기, 추상화

--- TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/Expression.java
+++ TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/Expression.java
@@ -2,4 +2,6 @@ package com.example.my_tdd_project;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+
+    Expression plus(Expression tenFrancs);
 }
--- TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/Money.java
+++ TDD/my-tdd-project/src/test/java/com/example/my_tdd_project/Money.java
@@ -26,7 +26,7 @@ class Money implements Expression {
         return currency;
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
     @Override
@@ -37,7 +37,7 @@ class Money implements Expression {
                 '}';
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
