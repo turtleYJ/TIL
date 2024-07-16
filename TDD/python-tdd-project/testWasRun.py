2c57ba8b50b7f136dc199f23a3248add7ec4e8f1
turtleYJ <yooil405@gmail.com>
2024-07-16 22:21:48 +0900

    feat(TDD): 20장 tearDown 테스트 통과 코드 작성

--- TDD/python-tdd-project/testWasRun.py
+++ TDD/python-tdd-project/testWasRun.py
@@ -3,24 +3,29 @@ class TestCase:
         self.name = name
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
-    def testMethod(self):
-        self.log = self.log + "testMethod "
     def setUp(self):
         self.log = "setUp "
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
         
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
     def testTemplateMethod(self):
         self.test.run()
-        assert(self.test.log == "setUp testMethod ")
+        assert(self.test.log == "setUp testMethod tearDown ")
         
 TestCaseTest("testTemplateMethod").run()
