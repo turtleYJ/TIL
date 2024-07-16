4e7d13c3f0aebca8d9e9de4b718373adaf07e80c
turtleYJ <yooil405@gmail.com>
2024-07-16 22:11:06 +0900

    feat(TDD): 20장 testMethode, testSetUp 병합 및 리팩터링

--- TDD/python-tdd-project/testWasRun.py
+++ TDD/python-tdd-project/testWasRun.py
@@ -1,40 +1,26 @@
 class TestCase:
     def __init__(self, name):
-        print("TestCase init")
         self.name = name
     def setUp(self):
-        print("TestCase setUp")
         pass
     def run(self):
-        print("TestCase run")
         self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     def __init__(self, name):
-        print("WasRun init")
         TestCase.__init__(self, name)
     def testMethod(self):
-        print("WasRun testMethod")
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
     def setUp(self):
-        print("WasRun setUp")
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
         
 class TestCaseTest(TestCase):
     def setUp(self):
-        print("TestCaseTest setUp")
         self.test = WasRun("testMethod")
-    def testRunning(self):
-        print("TestCaseTest testRunning")
+    def testTemplateMethod(self):
         self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
-        print("TestCaseTest testSetUp")
-        self.test.run()
-        assert(self.test.wasSetUp)
+        assert(self.test.log == "setUp testMethod ")
         
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
