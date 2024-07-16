0c795e1b5520d86e3a849bdfaa2b754f5aca183f
turtleYJ <yooil405@gmail.com>
2024-07-16 22:40:40 +0900

    feat(TDD): 21장 testResult 테스트 코드 구현중

--- TDD/python-tdd-project/testWasRun.py
+++ TDD/python-tdd-project/testWasRun.py
@@ -1,3 +1,11 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -6,10 +14,13 @@ class TestCase:
     def tearDown(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
 class WasRun(TestCase):
     def __init__(self, name):
@@ -20,6 +31,8 @@ class WasRun(TestCase):
         self.log = self.log + "testMethod "
     def tearDown(self):
         self.log = self.log + "tearDown "
+    def testBrokenMethod(self):
+        raise Exception
         
 class TestCaseTest(TestCase):
     def setUp(self):
@@ -27,5 +40,14 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         self.test.run()
         assert(self.test.log == "setUp testMethod tearDown ")
+    def testResult(self):
+        print(1)
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
         
 TestCaseTest("testTemplateMethod").run()
