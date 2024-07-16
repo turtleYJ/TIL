88d1a30072591ca8430cadfe87856dcb11cfcc45
turtleYJ <yooil405@gmail.com>
2024-07-16 21:39:12 +0900

    feat(TDD): 18장, 19장객체 생성 로직 공통화(setUp)

--- TDD/python-tdd-project/testWasRun.py
+++ TDD/python-tdd-project/testWasRun.py
@@ -1,22 +1,40 @@
 class TestCase:
     def __init__(self, name):
+        print("TestCase init")
         self.name = name
+    def setUp(self):
+        print("TestCase setUp")
+        pass
     def run(self):
+        print("TestCase run")
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
+        print("WasRun init")
         TestCase.__init__(self, name)
     def testMethod(self):
+        print("WasRun testMethod")
         self.wasRun = 1
+    def setUp(self):
+        print("WasRun setUp")
+        self.wasRun = None
+        self.wasSetUp = 1
         
 class TestCaseTest(TestCase):
+    def setUp(self):
+        print("TestCaseTest setUp")
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        print("TestCaseTest testRunning")
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        print("TestCaseTest testSetUp")
+        self.test.run()
+        assert(self.test.wasSetUp)
         
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
