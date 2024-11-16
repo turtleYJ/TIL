982a544092052a5b6faea1b8dc2c643751e78a5b
turtleYJ <yooil405@gmail.com>
2024-11-16 13:46:06 +0900

    feat(Spring): JPA 리퍼지터리 커스터마이징하기

--- Spring/Spring_in_Action/taco_cloud/src/main/java/tacos/data/OrderRepository.java
+++ Spring/Spring_in_Action/taco_cloud/src/main/java/tacos/data/OrderRepository.java
@@ -3,5 +3,11 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    // 스프링 데이터는 메서드 이름으로(findByDeliveryZip) deliveryZip속성과 일치히는 모든 개체를 찾아야한다는 걸 안다.
+    List<Order> findByDeliveryZip(String deliveryZip);
+
 }
