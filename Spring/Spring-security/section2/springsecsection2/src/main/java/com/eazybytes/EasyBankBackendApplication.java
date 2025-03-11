2d93aef9dd89b23634360baf84eb3ccfa56bc219
turtleYJ <yooil405@gmail.com>
2025-03-12 07:31:12 +0900

    feat(Spring): Spring Security section2
    * section2 init & 기본 컨트롤러 생성

---
+++ Spring/Spring-security/section2/springsecsection2/src/main/java/com/eazybytes/EasyBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankBackendApplication.class, args);
+	}
+
+}
