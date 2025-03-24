c45c64901d89ebf4e49b960a647b412eb42dbaa8
turtleYJ <yooil405@gmail.com>
2025-03-25 00:03:02 +0900

    feat(Spring): Spring Security section2
    * 요구 사항에 따라 보안 구성 변경 & fromLogin 및 httpBasic 인증 비활성화 테스트

---
+++ Spring/Spring-security/section2/springsecsection2/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.eazybytes.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
+        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                .requestMatchers("/notices", "/contact", "/error").permitAll());
+        http.formLogin((flc) -> flc.disable());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
