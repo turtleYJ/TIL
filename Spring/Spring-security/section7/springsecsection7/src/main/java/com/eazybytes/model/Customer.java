7d4165e173f80a79c6286aea82ee1a67395b7769
turtleYJ <yooil405@gmail.com>
2025-05-01 10:33:55 +0900

    feat(Spring): Spring Security section7
    * section7 init

---
+++ Spring/Spring-security/section7/springsecsection7/src/main/java/com/eazybytes/model/Customer.java
@@ -0,0 +1,21 @@
+package com.eazybytes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String pwd;
+    private String role;
+
+}
