0fc65340b8be56783023f1a8493adc04a6a47df7
turtleYJ <yooil405@gmail.com>
2025-06-13 21:05:10 +0900

    feat(Spring): Spring Security section9
    * 새 데이터베이스 테이블에서 권한을 로드하기 위한 백엔드 변경 사항 적용

--- Spring/Spring-security/section9/springsecsection9/src/main/java/com/eazybytes/filter/CsrfCookieFilter.java
+++ Spring/Spring-security/section9/springsecsection9/src/main/java/com/eazybytes/filter/CsrfCookieFilter.java
@@ -10,12 +10,13 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 public class CsrfCookieFilter extends OncePerRequestFilter {
+
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String cookieHeader = request.getHeader("Cookie");
-        System.out.println("==== [DEBUG] Cookie Header ====");
-        System.out.println(cookieHeader);  // null or actual content
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        // Render the token value to a cookie by causing the deferred token to be loaded
         csrfToken.getToken();
         filterChain.doFilter(request, response);
     }
